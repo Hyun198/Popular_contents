eab23628b4b972d27b5bcf911d28f2b1f3be1f6c
Hyun <hwn123h@naver.com>
2024-12-23 19:26:27 +0900

    api key

--- src/hooks/useYoutubeQuery.js
+++ src/hooks/useYoutubeQuery.js
@@ -2,12 +2,14 @@ import { useState, useEffect } from 'react';
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+
 const fetchPopularYoutube = () => {
-    return axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=movie%20trailer&type=video&maxResults=10&key=AIzaSyAYNUS7Bor5JWv7BaKxt5RsBucpcBk6bxE`)
+    return axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=movie%20trailer&type=video&maxResults=10&key=${API_KEY}`)
 }
 
 const fetchDetails = (videoIds) => {
-    return axios.get(`https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${videoIds.join(',')}&key=AIzaSyAYNUS7Bor5JWv7BaKxt5RsBucpcBk6bxE`)
+    return axios.get(`https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${videoIds.join(',')}&key=${API_KEY}`)
 }
 
 
