e84debf02f802a653525204d8b2fe8488c698b78
Hyun <hwn123h@naver.com>
2024-12-23 19:24:25 +0900

    총 조회수 chart

--- src/page/Youtube.jsx
+++ src/page/Youtube.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import { usePopularYoutubeQuery } from '../hooks/useYoutubeQuery'
+import { Line } from 'react-chartjs-2';
+import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, PointElement, CategoryScale, LinearScale } from 'chart.js';
+
+ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, PointElement, LinearScale);
 
 const Youtube = () => {
-
     const { popularVideos, videoDetails, isLoading, error } = usePopularYoutubeQuery();
-
-
+    console.log(videoDetails);
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+    if (error) {
+        return <div>데이터 불러오는 중에 에러가 발생했습니다. Contact로 연락주세요.😢</div>
+    }
+    //총 조회수
+    const chartData = {
+        labels: popularVideos?.map((video) => video.snippet.title), // X축
+        datasets: [
+            {
+                label: 'View Count',
+                data: videoDetails?.map((video) => parseInt(video.statistics.viewCount)), // Y축
+                fill: false,
+                borderColor: 'rgb(182, 50, 50)',
+                tension: 0.1,
+            },
+        ],
+    };
 
     return (
         <div>
@@ -17,6 +38,8 @@ const Youtube = () => {
                     </li>
                 ))}
             </ul>
+
+            <Line data={chartData} />
         </div>
     )
 }
